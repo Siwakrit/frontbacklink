e632568c90348c5a8dcb1f947d528a384d17494b
SIWAKRIT PATTAVEESIN <mr.siwakrit@gmail.com>
2024-12-05 00:17:24 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Quotes from './Quotes.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <header className="App-header">
+        <Quotes />
+      </header>
+    </div>
+  )
+}
+
+export default App
